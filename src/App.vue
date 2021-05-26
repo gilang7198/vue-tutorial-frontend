d4888d4c0c5e6000c4f202f48a2a456a35aefbe0
gilang7198 <gilang7198@gmail.com>
2021-05-27 05:52:38 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="primary"
+      dark
+    >
+      
+    </v-app-bar>
+
+    <v-main>
+      <Messages/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Messages from './components/Messages';
+
+export default {
+  name: 'App',
+
+  components: {
+    Messages,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/components/Messages.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h4>Messages</h4>
+        <ul v-for="items in messages" :key="items">
+            <li>{{ items }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            messages: [
+                "hello", "hi", "gilang"
+            ]
+        }
+    }
+}
+</script>
